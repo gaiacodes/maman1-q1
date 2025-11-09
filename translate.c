218e0e3a6eb395c5fa77a7603df3dc51cad2e91f
Gaia Koren <gaia.koren19@gmail.com>
2025-11-09 20:05:14 +0200

    make code clearer

--- translate.c
+++ translate.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
 
+#define MAX_DECIMAL_DIGITS 10
+#define MAX_BINARY_DIGITS 32
 
-void translate_dec(char decimal[]);
+// prints decimal number as binary
+void translate_dec(char decimal_str[]);
 
 
 int main(void) {
-    char decimal[11] = {0};
+    char decimal[MAX_DECIMAL_DIGITS + 1] = {0};
     printf("Enter a decimal number to convert to binary: ");
     scanf("%s", decimal);
     printf("Entered decimal number is: %s\n", decimal);
@@ -14,18 +17,18 @@ int main(void) {
     return 0;
 }
 
-void translate_dec(char decimal[]) {
-    unsigned int number;
-    char binary[33] = "00000000000000000000000000000000";
-    int binary_number_index = 31;
-    sscanf(decimal, "%u", &number);
+void translate_dec(char decimal_str[]) {
+    unsigned int decimal_number;
+    char binary[MAX_BINARY_DIGITS + 1] = "00000000000000000000000000000000";
+    int i = MAX_BINARY_DIGITS - 1;
+    sscanf(decimal_str, "%u", &decimal_number);
     do {
-        if (number % 2 == 1) {
-            binary[binary_number_index] = '1';
+        if (decimal_number % 2 == 1) {
+            binary[i] = '1';
         }
-        binary_number_index--;
-        number /= 2;
-    } while (number > 0);
+        i--;
+        decimal_number /= 2;
+    } while (decimal_number > 0);
 
-    printf("Binary representation is: %s\n", binary + binary_number_index + 1);
+    printf("Binary representation is: %s\n", binary + i + 1);
 }
